c268641b35d9b3b1a32ad344ac6991f73db99f76
WondwosenTsige <wondneh@gmail.com>
2021-10-06 10:42:04 -0700

    initial commit

---
+++ cookie_stands/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import CookieStand
+
+# Register your models here.
+@admin.register(CookieStand)
+class AdminForConcert(admin.ModelAdmin):
+    list_display = ("location", "owner", "created_at")
+  
